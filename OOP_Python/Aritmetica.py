de8fbf50f2dca4d9a8734587ce48856aafedaac5
IsraMejia <ihmaisrael@gmail.com>
2021-05-27 13:34:51 -0500

    Se creo la clase aritmetica, esta cool ue con :.2f defina que solo quiero dos numeros flotantes o decimales

---
+++ OOP_Python/Aritmetica.py
@@ -0,0 +1,27 @@
+class Aritmetica:
+    """Esta clase tiene codigo util para hacer operaciones aritmeticas """
+    def __init__(self, operandoA, operandoB):
+        self.operandoA = operandoA
+        self.operandoB = operandoB
+    
+    def sumar(self):
+        return self.operandoA + self.operandoB
+
+    def restar(self):
+        return self.operandoA - self.operandoB
+    
+    def multiplicar(self):
+        return self.operandoA * self.operandoB
+    
+    def dividir(self):
+        return self.operandoA / self.operandoB
+    
+
+print("\n\tEn este programa se crea la clase Aritmetica y se pone a prueba")
+
+aritmetica1 = Aritmetica(4, 3)
+print(f'La suma es : {aritmetica1.sumar()}')
+print(f'La resta es : {aritmetica1.restar()}')
+print(f'La multiplicacion es : {aritmetica1.multiplicar()}')
+print(f'La division es : {aritmetica1.dividir():.2f}') # con el :.2f solo se muestran dos numeros flotantes (decimales xd)
+        
--- OOP_Python/Persona.py
+++ OOP_Python/Persona.py
@@ -1,7 +1,7 @@
 class Persona:
     def __init__(self, nombre, apellido, edad): 
     #El parametro 'self' al metodo inicializador de la clase hace referencia al objeto mismo que se va a crear
-        #El doble guion bajo 'dunder' le da el nombre de metodo dunder init
+        #El doble guion bajo 'dunder' le da el nombre de metodo "dunder init"
         self.nombre =  nombre #self.Atributo de instancia, del objeto que estamos creando por defecto
         self.apellido  =  apellido  #Atributo = parametro
         self.edad =  edad
@@ -25,7 +25,10 @@ persona1 = Persona("Isra" , "Mejia" , 23) #Los parentesis manda a llamar el meto
 # persona1.edad = 60
 # print(f"El nombre de la persona1 es: {persona1.nombre} {persona1.apellido} \ny su edad es: {persona1.edad} \n")
 persona1.mostrar_detalle()
+persona1.carrera = "Ingenieria en Computacion"
+print(f"Y su carrera es {persona1.carrera}\n\n") #Se le pueden crear atributos dentro de la marcha, pero esto no se aplica a todas las clases, solo al objeto en cuestion
 
 persona2 = Persona("Rex", "PerroBeibe", 7)
 # print(f"El nombre de la persona 2 es: {persona2.nombre} {persona2.apellido} \ny su edad es: {persona2.edad} \n")
-persona2.mostrar_detalle()
+Persona.mostrar_detalle(persona2) #Podemos usar el nombre de la clase y pasamos como parametro el objeto para que jale 
+# print(f"Y es {persona2.carrera}") #No tiene definido el atributo carrera, se puede definir durante la marcha al objeto, pero no se hizo
