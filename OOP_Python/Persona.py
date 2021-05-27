e774b30556889e4d2c36d100b43f72a5b57cb2ca
IsraMejia <ihmaisrael@gmail.com>
2021-05-27 13:08:22 -0500

    parametro self, similar al this o widget en flutter

--- OOP_Python/Persona.py
+++ OOP_Python/Persona.py
@@ -1,14 +1,31 @@
 class Persona:
-    def __init__(self): #El parametro 'self' al metodo inicializador de la clase hace referencia al objeto mismo que se va a crear
+    def __init__(self, nombre, apellido, edad): 
+    #El parametro 'self' al metodo inicializador de la clase hace referencia al objeto mismo que se va a crear
         #El doble guion bajo 'dunder' le da el nombre de metodo dunder init
-        self.nombre = 'Rex' #Atributo de instancia, del objeto que estamos creando por defecto
-        self.apellido  = 'Mejia'
-        self.edad = 7
+        self.nombre =  nombre #self.Atributo de instancia, del objeto que estamos creando por defecto
+        self.apellido  =  apellido  #Atributo = parametro
+        self.edad =  edad
+    
+    def mostrar_detalle(self): #Se necesita el parametro "self" para funcionar como metodo de instancia
+        print(f"El nombre de la persona es:\n {self.nombre} {self.apellido} \n y su edad es: {self.edad} \n")
+            #Con la palabra self nos referimos al objeto en cuestion como cuando usamos widget en flutter :D
+
+
     #pass #permite generar la clase sin  que se le defina atributos o metodos
 
-print("\nLeccion 01 POO\n")
+
+print("\n\tLeccion 01 POO\n")
 print(type(Persona))
 
-persona1 = Persona() #Los parentesis manda a llamar el metodo inicializador o constructor de la clase __init__
+persona1 = Persona("Isra" , "Mejia" , 23) #Los parentesis manda a llamar el metodo inicializador o constructor de la clase __init__
 
-print(f"El nombre de la persona es: {persona1.nombre} {persona1.apellido} \ny su edad es: {persona1.edad} \n")
+# print(f"El nombre de la persona1 es: {persona1.nombre} {persona1.apellido} \ny su edad es: {persona1.edad} \n")
+
+# persona1.nombre = "Hipolito"
+# persona1.edad = 60
+# print(f"El nombre de la persona1 es: {persona1.nombre} {persona1.apellido} \ny su edad es: {persona1.edad} \n")
+persona1.mostrar_detalle()
+
+persona2 = Persona("Rex", "PerroBeibe", 7)
+# print(f"El nombre de la persona 2 es: {persona2.nombre} {persona2.apellido} \ny su edad es: {persona2.edad} \n")
+persona2.mostrar_detalle()
