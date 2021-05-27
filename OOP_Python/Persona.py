89575e8c3c68a96ad2ad938bd53584313b34e4dc
IsraMejia <ihmaisrael@gmail.com>
2021-05-27 14:33:24 -0500

    Encapsulamiento con _ (buena practica) y por __ (buena practica y de forma automatica por el lenguaje)

--- OOP_Python/Persona.py
+++ OOP_Python/Persona.py
@@ -1,38 +1,65 @@
-class Persona:
-    def __init__(self, nombre, apellido, edad, *tupla, **diccionario): 
-        #   *args = argumentos variables de tipo tupla
-        #   **kwargs = argumentos variables de tipo diccionario
-    #El parametro 'self' al metodo inicializador de la clase hace referencia al objeto mismo que se va a crear
-        #El doble guion bajo 'dunder' le da el nombre de metodo "dunder init"
-        self.nombre =  nombre #self.Atributo de instancia, del objeto que estamos creando por defecto
-        self.apellido  =  apellido  #Atributo = parametro
-        self.edad =  edad
-        self.tupla = tupla
-        self.diccionario = diccionario
+# class Persona:
+#     def __init__(self, nombre, apellido, edad, *tupla, **diccionario): 
+#         #   *args = argumentos variables de tipo tupla
+#         #   **kwargs = argumentos variables de tipo diccionario
+#     #El parametro 'self' al metodo inicializador de la clase hace referencia al objeto mismo que se va a crear
+#         #El doble guion bajo 'dunder' le da el nombre de metodo "dunder init"
+#         self.__nombre =  nombre #self.Atributo de instancia, del objeto que estamos creando por defecto
+#         self.apellido  =  apellido  #Atributo = parametro
+#         self.edad =  edad
+#         self.tupla = tupla
+#         self.diccionario = diccionario
     
-    def mostrar_detalle(self): #Se necesita el parametro "self" para funcionar como metodo de instancia
-        print(f"El nombre de la persona es:\n {self.nombre} {self.apellido} \n  su edad es: {self.edad} \n Su tupla es {self.tupla} \n y su diccionario es {self.diccionario}")
-            #Con la palabra self nos referimos al objeto en cuestion como cuando usamos widget en flutter :D
+#     def mostrar_detalle(self): #Se necesita el parametro "self" para funcionar como metodo de instancia
+#         print(f"El nombre de la persona es:\n {self.__nombre} {self.apellido} \n  su edad es: {self.edad} \n Su tupla es {self.tupla} \n y su diccionario es {self.diccionario}")
+#             #Con la palabra self nos referimos al objeto en cuestion como cuando usamos widget en flutter :D
 
 
-    #pass #permite generar la clase sin  que se le defina atributos o metodos
+#     #pass #permite generar la clase sin  que se le defina atributos o metodos
 
 
-print("\n\tLeccion 01 POO\n")
-print(type(Persona))
+# print("\n\tLeccion 01 POO\n")
+# print(type(Persona))
 
-persona1 = Persona("Isra" , "Mejia", 23,   3178,45,321,32, m='manzana', a = 'apple' ) #Los parentesis manda a llamar el metodo inicializador o constructor de la clase __init__
+# persona1 = Persona("Isra" , "Mejia", 23,   3178,45,321,32, m='manzana', a = 'apple' ) #Los parentesis manda a llamar el metodo inicializador o constructor de la clase __init__
 
-# print(f"El nombre de la persona1 es: {persona1.nombre} {persona1.apellido} \ny su edad es: {persona1.edad} \n")
+# # print(f"El nombre de la persona1 es: {persona1.nombre} {persona1.apellido} \ny su edad es: {persona1.edad} \n")
 
-# persona1.nombre = "Hipolito"
-# persona1.edad = 60
-# print(f"El nombre de la persona1 es: {persona1.nombre} {persona1.apellido} \ny su edad es: {persona1.edad} \n")
-persona1.mostrar_detalle()
-persona1.carrera = "Ingenieria en Computacion"
-print(f"Y su carrera es {persona1.carrera}\n\n") #Se le pueden crear atributos dentro de la marcha, pero esto no se aplica a todas las clases, solo al objeto en cuestion
+# # persona1.nombre = "Hipolito"
+# # persona1.edad = 60
+# # print(f"El nombre de la persona1 es: {persona1.nombre} {persona1.apellido} \ny su edad es: {persona1.edad} \n")
+# persona1.mostrar_detalle()
+# persona1.carrera = "Ingenieria en Computacion"
+# print(f"Y su carrera es {persona1.carrera}\n\n") #Se le pueden crear atributos dentro de la marcha, pero esto no se aplica a todas las clases, solo al objeto en cuestion
 
-persona2 = Persona("Rex", "PerroBeibe", 7)
-# print(f"El nombre de la persona 2 es: {persona2.nombre} {persona2.apellido} \ny su edad es: {persona2.edad} \n")
-Persona.mostrar_detalle(persona2) #Podemos usar el nombre de la clase y pasamos como parametro el objeto para que jale 
-# print(f"Y es {persona2.carrera}") #No tiene definido el atributo carrera, se puede definir durante la marcha al objeto, pero no se hizo
+# persona2 = Persona("Rex", "PerroBeibe", 7)
+# # print(f"El nombre de la persona 2 es: {persona2.nombre} {persona2.apellido} \ny su edad es: {persona2.edad} \n")
+# Persona.mostrar_detalle(persona2) #Podemos usar el nombre de la clase y pasamos como parametro el objeto para que jale 
+# # print(f"Y es {persona2.carrera}") #No tiene definido el atributo carrera, se puede definir durante la marcha al objeto, pero no se hizo
+
+
+# #----------------------Encapsulamiento 
+# # al poner "__" en las variables de instancia, se entiende que son privadas, y asi tenemos que usarlas
+# persona1.__nombre = 'Hipolitooo' #NO se recomieda hacer esto
+# print(f'Nombre = {persona1.__nombre}') 
+# #Al poner el "__" el metodo mostrar detalle deja de servir, asi que se podria decir que si sirve, pero es por buenas practicas de codigo
+# persona1.mostrar_detalle()
+
+class Persona:
+    def __init__(self, nombre, apellido, edad):   
+        #El doble guion bajo 'dunder' le da el nombre de metodo "dunder init"
+        self.__nombre =  nombre #self.Atributo de instancia, del objeto que estamos creando por defecto
+        self.apellido  =  apellido  #Atributo = parametro
+        self.edad =  edad 
+    
+    def mostrar_detalle(self):  
+        print(f"El nombre de la persona es:\n {self.__nombre} {self.apellido} \n su edad es: {self.edad}")
+  
+persona1 = Persona("Isra" , "Mejia", 23)
+
+#----------------------Encapsulamiento 
+# al poner "__" en las variables de instancia, se entiende que son privadas, y asi tenemos que usarlas
+persona1.__nombre = 'Hipolitooo' #NO se recomieda hacer esto
+print(f'Nombre = {persona1.__nombre}') #Hace referencia a __nombre, pero NO a _Persona__nombre = 'Isra'
+#Al poner el "__" el metodo mostrar detalle deja de servir, asi que se podria decir que si sirve, pero es por buenas practicas de codigo
+persona1.mostrar_detalle()  
