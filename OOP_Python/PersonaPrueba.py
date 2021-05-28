ff1dee878e58e1d1a78b19e83d7c330cdc63a3b1
IsraMejia <ihmaisrael@gmail.com>
2021-05-27 22:02:18 -0500

    Se usa el modulo/archivo Persona y su clase Persona, pero ejecuta todo el demas codigo tambien, en el siguiente commit se va a solucionar

---
+++ OOP_Python/PersonaPrueba.py
@@ -0,0 +1,5 @@
+from Persona import Persona 
+#Del modulo/archivo Persona, importamos la clase Persona   . importar todas las clases es con '*'
+
+persona1 = Persona('Bella', 'Poarch', 23)
+persona1.mostrar_detalle()
