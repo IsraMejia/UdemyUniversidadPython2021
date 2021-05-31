07368d251156dac7242fa06ef4d01ed5a990dbf1
IsraMejia <ihmaisrael@gmail.com>
2021-05-30 22:45:47 -0500

    Validaciones setters, estructura ternaria

--- OOP_Python/HerenciaMultiple/FiguraGeometrica.py
+++ OOP_Python/HerenciaMultiple/FiguraGeometrica.py
@@ -1,7 +1,16 @@
 class FiguraGeometrica:
     def __init__(self, ancho, alto):
-        self._ancho = ancho
-        self._alto = alto
+        if self._validar_valor(ancho):
+            self._ancho = ancho
+        else:
+            self.ancho = 0
+            print(f"Valor erroneo ancho: {ancho}")
+
+        if self._validar_valor(alto) :
+            self._alto = alto
+        else:
+            self._alto = 0
+            print(f"Valor erroneo alto : {alto}")
     
     @property #getter
     def ancho(self):
@@ -9,7 +18,10 @@ class FiguraGeometrica:
     
     @ancho.setter
     def ancho(self, ancho):
-        self._ancho = ancho
+        if self._validar_valor(ancho):
+            self._ancho = ancho
+        else:
+            print(f"Valor erroneo ancho: {ancho} , se mantiene el valor previo a esta modificacion fallida")
 
     @property
     def alto(self):
@@ -17,9 +29,18 @@ class FiguraGeometrica:
 
     @alto.setter
     def alto(self, alto):
-        self._alto = alto    
+        if self._validar_valor(alto):
+            self._ancho = ancho
+        else:
+            print(f"Valor erroneo ancho: {alto} , se mantiene el valor previo a esta modificacion fallida")
 
     
     def __str__(self):
         return f"class FiguraGeometrica [ancho: {self._ancho}, alto: {self._alto}] "
+
+    def _validar_valor(self, valor):
+        """ es un numero mayor a 0 y menor a 10?"""
+        return True  if(0 < valor < 10)   else False
+
+        
     
--- OOP_Python/HerenciaMultiple/test_figura_geometrica.py
+++ OOP_Python/HerenciaMultiple/test_figura_geometrica.py
@@ -1,7 +1,7 @@
 from Cuadrado import Cuadrado
 from Rectangulo import Rectangulo
-
-cuadrado1 = Cuadrado(3, 'azul')
+print('Creacion de Objeto Cuadrado'.center(70, '-'))
+cuadrado1 = Cuadrado(lado=9, color='azul')
 print(f"\nEl alto es: {cuadrado1.alto} , su ancho es: {cuadrado1.ancho}, su color es {cuadrado1.color}")
 print(f"Y su area es: { cuadrado1.calcular_area() }")
 
@@ -9,6 +9,7 @@ print(f"Y su area es: { cuadrado1.calcular_area() }")
 # print(Cuadrado.mro()) # Sigue el orden en el que se definio la herencia, primero la clase hija, luego la primer clase de herencia , luego la segunda y asi
 print(f"\n{cuadrado1}\n") 
 
-rectangulo1 = Rectangulo(2, 3, 'Purpura')
-print(f"\n{rectangulo1}\n") 
-print(f"Área del rectangulo:  { rectangulo1.calcular_area() }")
+rectangulo1 = Rectangulo(ancho=-2 , alto= 3, color= 'Purpura')
+rectangulo1.ancho = 4   
+print(f"\n{rectangulo1}\n")
+print(f"Área del rectangulo:  { rectangulo1.calcular_area() }")   
